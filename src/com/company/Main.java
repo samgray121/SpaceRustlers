1f2c64bbab6b4c6c51106d9caab6719494a37326
sam.gray121@gmail.com <Mrtwister123!>
2022-01-06 08:23:06 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int spaceships = 10;
+        int aliens = 25;
+        int cows = 100;
+
+        if(aliens > spaceships){
+            System.out.println("Vroom, vroom! Let's get going!");
+        } else{
+            System.out.println("There aren't enough green guys to drive these ships!");
+
+        }
+
+        if(cows == spaceships){
+            System.out.println("Wow, way to plan ahead! Just enough room for all these walking hamburgers!");
+        } else if (cows > spaceships) {
+            System.out.println("Dangit! I don't know how we're going to fit all these cows in here!");
+        } else {
+            System.out.println("Too many ships! Not enough cows.");
+        }
+
+        if(cows < aliens) {
+            System.out.println("Hurrah, we've got the grub! Hamburger party on Alpha Centauri!");
+        } else if (cows >= aliens) {
+            System.out.println("Oh no! The herds got restless and took over! Looks like _we're_ hamburger now!");
+        }
+
+
+
+
+    }
+}
